32064ab71540c896e1dab0c6a1a9e5c424869b46
Vincent P <vincent.poulain2@gmail.com>
2019-09-11 13:51:44 +0200

    Add CSS initial requirements (#8)
    * Add CSS initial requirements
    
    * Add Grommet instead of initial basic CSS
    
    * Add tslint-react & apply prettier rules
    
    * Fix typos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
 import React from 'react'
-import './App.css'
+import BookCard from './components/bookCard'
+import books from './data/books'
 
-function foo() {
-    return 'Vincent'
+import { Box, Grid, Grommet, Heading } from 'grommet'
+import { Book } from './types'
+
+const theme = {
+    global: {
+        font: {
+            family: 'Roboto',
+            height: '20px',
+            size: '14px',
+        },
+    },
 }
 
+const AppBar = (props: any) => (
+    <Box
+        tag="header"
+        direction="row"
+        align="center"
+        justify="between"
+        background="brand"
+        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+        elevation="medium"
+        {...props}
+    />
+)
+
 const App: React.FC = () => {
-    return <div className="App">Hello {foo()}</div>
+    return (
+        <Grommet theme={theme} full={true}>
+            <Box fill>
+                <AppBar>
+                    <Heading level="3" margin="none">
+                        Hello, I'm Vincent
+                    </Heading>
+                </AppBar>
+                <Box
+                    direction="row"
+                    align="center"
+                    justify="center"
+                    margin="30px 0px 0px 0px"
+                >
+                    <Grid
+                        rows={['small', 'small']}
+                        columns={['xsmall', 'xsmall', 'xsmall']}
+                        justify="center"
+                        align="center"
+                        gap="xsmall"
+                    >
+                        {books.map((item: Book) => {
+                            return <BookCard key={0} {...item} />
+                        })}
+                    </Grid>
+                </Box>
+            </Box>
+        </Grommet>
+    )
 }
 
 export default App
