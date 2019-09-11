2743c3a05e93d8a94af203f4144503fb462e1741
Vincent Poulain <vincent.poulain2@gmail.com>
2019-09-11 13:27:02 +0200

    Add Grommet instead of initial basic CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,62 @@
 import React from 'react'
-import './App.css'
-import BookCard from './components/bookCard'
+import BookCard from './components/BookCard'
 import books from './data/books'
+
+import { Box, Button, Grommet, Grid, Heading } from 'grommet'
 import { Book } from './types'
+
+const theme = {
+    global: {
+        font: {
+            family: 'Roboto',
+            height: '20px',
+            size: '14px',
+        },
+    },
+}
+
+const AppBar = (props: any) => (
+    <Box
+        tag="header"
+        direction="row"
+        align="center"
+        justify="between"
+        background="brand"
+        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+        elevation="medium"
+        {...props}
+    />
+)
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <h1> Hello, I'm Vincent </h1>
-            {books.map((item: Book) => {
-                return <BookCard key={0} {...item} />
-            })}
-        </div>
+        <Grommet theme={theme} full={true}>
+            <Box fill>
+                <AppBar>
+                    <Heading level="3" margin="none">
+                        Hello, I'm Vincent
+                    </Heading>
+                </AppBar>
+                <Box
+                    direction="row"
+                    align="center"
+                    justify="center"
+                    margin="30px 0px 0px 0px"
+                >
+                    <Grid
+                        rows={['small', 'small']}
+                        columns={['xsmall', 'xsmall', 'xsmall']}
+                        justify="center"
+                        align="center"
+                        gap="xsmall"
+                    >
+                        {books.map((item: Book) => {
+                            return <BookCard key={0} {...item} />
+                        })}
+                    </Grid>
+                </Box>
+            </Box>
+        </Grommet>
     )
 }
 
