d080f5bf3c3c44c46a12d3e2d09c075fb509f9a4
Vincent Poulain <vincent.poulain2@gmail.com>
2019-09-16 11:52:45 +0200

    Add Main layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,24 @@ const artWork = require('./static/images/artworks/artwork-1.svg')
 const App: React.FC = () => {
     return (
         <Grommet theme={theme} full>
-            <Box direction="column">
+            <Box direction="column" align="center">
                 <Box
                     direction="row"
                     height="xsmall"
                     justify="end"
                     overflow="visible"
+                    width="full"
                 >
                     <Box>
                         <Image src={artWork} />
                     </Box>
                 </Box>
-                <Box direction="row" justify="center">
-                    <Box width="xlarge">
+                <Box direction="row" justify="center" width="xlarge">
+                    <Box>
                         <Sidenav />
+                    </Box>
+                    <Box>
+                        <Main />
                     </Box>
                 </Box>
             </Box>
--- src/components/sidenav/Nav.tsx
+++ src/components/sidenav/Nav.tsx
@@ -12,43 +12,41 @@ const imageProfileUrl = require('../../static/images/profiles/stiegler.jpg')
 const Sidenav: React.FC = () => {
     return (
         <Box direction="column">
-            <Box>
-                <Box width="xmedium">
+            <Box width="xmedium">
+                <Box direction="column">
                     <Box direction="column">
-                        <Box direction="column">
-                            <PictureProfile imageUrl={imageProfileUrl} />
-                            <Heading
-                                level={1}
-                                color="dark-1"
-                                margin={{ top: '16px', bottom: '0px' }}
-                            >
-                                Hello,
-                            </Heading>
-                            <Heading
-                                level={2}
-                                size="large"
-                                color="dark-1"
-                                margin={{ vertical: '0px' }}
-                            >
-                                I'm Vincent
-                            </Heading>
-                        </Box>
+                        <PictureProfile imageUrl={imageProfileUrl} />
+                        <Heading
+                            level={1}
+                            color="dark-1"
+                            margin={{ top: '16px', bottom: '0px' }}
+                        >
+                            Hello,
+                        </Heading>
+                        <Heading
+                            level={2}
+                            size="large"
+                            color="dark-1"
+                            margin={{ vertical: '0px' }}
+                        >
+                            I'm Vincent
+                        </Heading>
                     </Box>
-                    <Box
-                        direction="column"
-                        margin={{ top: '40px', bottom: '10px' }}
-                    >
-                        <Separator color={theme.global.colors.brand} />
-                    </Box>
-                    <Heading level={3} size="large" color="dark-1">
-                        I like building things.
-                        <br />
-                        My primary job function is as a software engineer.
-                        <br />I care about how technology can support well-being
-                        and global challenges.
-                    </Heading>
-                    <Social />
                 </Box>
+                <Box
+                    direction="column"
+                    margin={{ top: '40px', bottom: '10px' }}
+                >
+                    <Separator color={theme.global.colors.brand} />
+                </Box>
+                <Heading level={3} size="large" color="dark-1">
+                    I like building things.
+                    <br />
+                    My primary job function is as a software engineer.
+                    <br />I care about how technology can support well-being and
+                    global challenges.
+                </Heading>
+                <Social />
             </Box>
         </Box>
     )
