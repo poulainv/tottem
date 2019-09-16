5dd58e5d8c7715bf7cc6269f98ffaae489646fad
Vincent Poulain <vincent.poulain2@gmail.com>
2019-09-16 11:44:06 +0200

    Add artwork header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { Box, Grid, Grommet } from 'grommet'
+import { Box, Grid, Grommet, Stack, Image } from 'grommet'
 import React from 'react'
 import Main from './components/Layouts/Main'
 import Sidenav from './components/sidenav/Nav'
 import theme from './theme'
 
+const artWork = require('./static/images/artworks/artwork-1.svg')
+
 const App: React.FC = () => {
     return (
         <Grommet theme={theme} full>
-            <Box direction="row" justify="center">
-                <Box width="xlarge" margin={{ top: 'xlarge' }}>
-                    <Sidenav />
+            <Box direction="column">
+                <Box
+                    direction="row"
+                    height="xsmall"
+                    justify="end"
+                    overflow="visible"
+                >
+                    <Box>
+                        <Image src={artWork} />
+                    </Box>
+                </Box>
+                <Box direction="row" justify="center">
+                    <Box width="xlarge">
+                        <Sidenav />
+                    </Box>
                 </Box>
             </Box>
         </Grommet>
