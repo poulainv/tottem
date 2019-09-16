acc3f6efa617514f14f1aa955b466d86ef6a3a23
Vincent Poulain <vincent.poulain2@gmail.com>
2019-09-16 15:10:37 +0200

    Add markdown capability to collection header

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Box, Heading } from 'grommet'
+import { Box, Heading, Markdown } from 'grommet'
 import ItemList from './ItemList'
 
 const Collection: React.FC = () => {
@@ -8,10 +8,10 @@ const Collection: React.FC = () => {
         <Box direction="column" fill="vertical" margin={{ top: '70px' }}>
             <Box>
                 <Heading level="2" size="medium">
-                    Books, that get me inspired
+                    <Markdown>**Books,** that get me inspired</Markdown>
                 </Heading>
             </Box>
-            <Box background="light-3">
+            <Box>
                 <ItemList />
             </Box>
         </Box>
