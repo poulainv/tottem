247a5eeed477acd733bb44f180acbb15afa41225
Vincent P <vincent.poulain2@gmail.com>
2019-09-17 19:32:58 +0200

    Define UserProfile as dynamic data (#14)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import Main from './components/Layouts/Main'
 import Sidenav from './components/sidenav/Nav'
 import theme from './theme'
+import profiles from './data/profiles'
 
 const artWork = require('./static/images/artworks/artwork-1.svg')
+const user = profiles[0]
 
 const App: React.FC = () => {
     return (
@@ -28,7 +30,7 @@ const App: React.FC = () => {
                     gap="large"
                 >
                     <Box>
-                        <Sidenav />
+                        <Sidenav {...user} />
                     </Box>
                     <Box>
                         <Main />
