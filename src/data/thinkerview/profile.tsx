1d71739f5bccf250ad6c653d30541fd3df89087b
Vincent Poulain <vincent.poulain2@gmail.com>
2019-09-18 20:02:56 +0200

    Fix thinkerview biography typo

--- src/data/thinkerview/profile.tsx
+++ src/data/thinkerview/profile.tsx
@@ -8,7 +8,7 @@ const profile: UserProfile = {
         [ThinkerView](https://thinkerview.com) est un laboratoire d'idées français indépendant lancé en janvier 2013, proche du milieu du hacking.<br> \
         Il diffuse de longues entrevues sans montage, avec des personnalités d'horizons différents, \
         principalement grâce à sa chaîne YouTube suivie par plus de 400 000 abonnés. <br> <br> \
-        Chaque invité est convié à recommander une série de livres, les voicis ",
+        _Chaque invité est convié à recommander une série de livres, les voici_ ",
 }
 
 export default profile
