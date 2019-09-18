a8afebe27c0a66edd9c991f707d85dc9dca5ce42
Vincent Poulain <vincent.poulain2@gmail.com>
2019-09-18 15:25:37 +0200

    Add thinkerview profile

---
+++ src/data/thinkerview/sections.tsx
@@ -0,0 +1,24 @@
+import { Section } from '../../types'
+
+const sections: Section[] = [
+    {
+        id: '1',
+        name: 'Finance',
+        index: 0,
+    },
+    {
+        id: '2',
+        name: 'Société',
+        index: 1,
+    },
+    {
+        id: '3',
+        name: 'Géopolitique',
+        index: 2,
+    },
+    {
+        id: '4',
+        name: 'Climat',
+        index: 3,
+    },
+]
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,18 +1,29 @@
 export interface ICollection {
+    id: string
     name: string
     items: Item[]
 }
 
+export interface Section {
+    id: string
+    name: string
+    index: number
+}
+
 export interface Item {
+    collectionId: string
     author: string
     imageUrl: string
     title: string
     imageColor?: string
+    productUrl: string
+    note?: string
+    type: string
+    detail?: string
 }
 
 export interface Book extends Item {
-    imageColor: string
-    amazonUrl: string
+    imageColor?: string
 }
 
 export interface Album extends Item {
