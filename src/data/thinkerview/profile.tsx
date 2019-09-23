3297e39f1b668b7c42cb2930efb572687595f4ce
Vincent Poulain <vincent.poulain2@gmail.com>
2019-09-23 20:19:30 +0200

    Remove biography markdown

--- src/data/thinkerview/profile.tsx
+++ src/data/thinkerview/profile.tsx
@@ -2,10 +2,10 @@ import { UserProfile } from '../../types'
 
 const profile: UserProfile = {
     firstname: 'Thinkerview',
-    pictureUrl: require('../../static/images/profiles/thinkerview.png'),
-    biography: `[ThinkerView](https://thinkerview.com) est un laboratoire d'idées français indépendant lancé en janvier 2013, proche du milieu du hacking.<br> 
-        Il diffuse de longues entrevues sans montage, avec des personnalités d'horizons différents, <br> <br> 
-        _Chaque invité est convié à recommander une série de livres, les voici_ `,
+    pictureUrl: require('../../static/images/profiles/thinkerview.jpg'),
+    biography: `ThinkerView est un laboratoire d'idées français indépendant. Il diffuse de longues entrevues sans montage, 
+        avec des personnalités d'horizons différents. 
+        Chaque invité est convié à recommander une série de livres, les voici`,
 }
 
 export default profile
