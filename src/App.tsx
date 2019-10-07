bfbb183cc1d6c482fc1f965d2f3be8c8a4a4ad97
Vincent Poulain <vincent.poulain2@gmail.com>
2019-10-07 12:38:45 +0200

    Use hooks to init ReactGA

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Grommet, ResponsiveContext } from 'grommet'
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Profile from './pages/Profile'
 import theme from './theme'
 import ReactGA from 'react-ga'
 
-ReactGA.initialize('UA-149517534-1')
-ReactGA.pageview(window.location.pathname + window.location.search)
-
 const App: React.FC = () => {
+    useEffect(() => {
+        ReactGA.initialize('UA-149517534-1')
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    })
+
     return (
         <Router>
             <Grommet theme={theme}>
