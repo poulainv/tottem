c0683d429cb7bbdc5ddcd6c7f897aa06aa2720f4
Vincent Poulain <vincent.poulain2@gmail.com>
2019-10-08 16:02:33 +0200

    Make username profile non case sensitive

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -11,7 +11,7 @@ interface RoutingMatchProps extends RouteComponentProps<RoutingMatchParams> {}
 
 const Profile = (props: RoutingMatchProps) => {
     // Fetch data
-    const userProfile: UserProfile = require(`./../data/${props.match.params.profileId}/profile`)
+    const userProfile: UserProfile = require(`./../data/${props.match.params.profileId.toLocaleLowerCase()}/profile`)
         .default
     const sections = require(`../data/${props.match.params.profileId}/sections`)
         .default
