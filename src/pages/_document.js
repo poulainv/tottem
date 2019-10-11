234a4dc1f0d7018abff82f52cc3b751d6d482636
Vincent Poulain <vincent.poulain2@gmail.com>
2019-10-11 20:24:07 +0200

    Customize _add.js with Default SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@
 // ./pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-import { DefaultSeo } from 'next-seo'
 
 class MyDocument extends Document {
     static async getInitialProps({ renderPage }) {
@@ -20,29 +19,8 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head />
-                <DefaultSeo
-                    title="Tottem"
-                    description="Knowledge platform for community, it's place where enthusiastic people and organizations share relevant collections of hand-picked items — books, articles, movies and more"
-                    openGraph={{
-                        type: 'website',
-                        locale: 'en_en',
-                        url: 'htts://tottem.app',
-                        site_name: 'Tottem',
-                        images: [
-                            {
-                                width: 698,
-                                height: 388,
-                                url: 'https://tottem.app/thumbnail.png',
-                            },
-                        ],
-                    }}
-                    twitter={{
-                        site: '@TottemApp',
-                        cardType: 'summary_large_image',
-                    }}
-                />
                 {this.props.styleTags}
-                <meta charset="utf-8" />
+                <meta charSet="utf-8" />
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <meta
                     name="viewport"
