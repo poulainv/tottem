c90ccdc2b91bf7039cf96b2b18e6db6a8a9a36c0
Vincent Poulain <vincent.poulain2@gmail.com>
2019-10-11 20:01:24 +0200

    Fix humanetech biography

--- src/data/humanetech/profile.tsx
+++ src/data/humanetech/profile.tsx
@@ -6,7 +6,7 @@ const profile: UserProfile = {
     biography: `The Center for Humane Technology (CHT) is an independent nonprofit 
     organization whose aim is to drive a comprehensive shift toward humane technology by changing the way 
     technologists think about their work and how they build products.
-    We’re creating the conditions for a new race to the top to realign technology with humanity.`,
+    [They are] creating the conditions for a new race to the top to realign technology with humanity.`,
     label: 'unofficial',
     social: {
         website: 'https://humanetech.com/',
