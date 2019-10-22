7d988f05b7f5f1b1aa84e397675fbb8ec94bf542
Vincent Poulain <vincent.poulain2@gmail.com>
2019-10-22 19:26:28 +0200

    Add beta on logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 
-const Logo = styled.h1`
+export const Beta = styled.p`
+    font-size: 12px;
+    position: absolute;
+    margin: 0px;
+    line-height: 100%;
+    top: 0px;
+    right: -26px;
+`
+
+export const Logo = styled.h1`
+    position: relative;
     font-size: 30px;
     color: black;
     font-family: 'Poiret One';
