1c9c91af34fd7a8f17eb59b2066c706e8b114b1d
Vincent Poulain <vincent.poulain2@gmail.com>
2019-10-22 10:39:25 +0200

    Use absolute position for back button to ensure layout alignment

--- src/pages/[profile]/collection/[collectionId].tsx
+++ src/pages/[profile]/collection/[collectionId].tsx
@@ -27,6 +27,9 @@ const BackButton = styled(Box)`
     border-radius: 50%;
     width: 40px;
     height: 40px;
+    position: absolute;
+    top: 0px;
+    left: -32px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
 `
 
@@ -54,7 +57,11 @@ const Collection: NextPage<ICollectionProps> = ({
                     ],
                 }}
             />
-            <Box direction="row" margin={{ top: 'large' }}>
+            <Box
+                direction="row"
+                margin={{ top: 'large' }}
+                style={{ position: 'relative' }}
+            >
                 <Link href="/[profile]" as={`/${router.query.profile}`}>
                     <BackButton>
                         <LinkPrevious
