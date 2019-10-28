bf8fc1af62cd18b4af57ad7237fb6987840e0c50
Vincent Poulain <vincent.poulain2@gmail.com>
2019-10-28 12:06:37 +0100

    Use h2 instead h3 for collection name

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -73,7 +73,7 @@ export const ElementAuthor = styled.p`
     }
 `
 
-export const CollectionTitle = styled.h3`
+export const CollectionTitle = styled.h2`
     font-size: 16px;
     font-weight: 400;
     cursor: pointer;
