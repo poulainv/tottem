d4501e19abac0bfad3aa05d7ca6ff035ddca1302
cdeon <clement.deon@data-publica.com>
2019-11-01 13:40:28 +0100

    #61 Set Auth0 config as env variables (and as Now secrets)

--- src/lib/auth0-variables.js
+++ src/lib/auth0-variables.js
@@ -1,7 +1,7 @@
 const domain =
     process.env.ENV === 'dev' ? 'http://localhost:3000' : 'https://tottem.app'
 export const AUTH_CONFIG = {
-    domain: 'dev-cajmwjmi.eu.auth0.com',
-    clientId: 'tHd8WdWpAXY7B7oZ1M2MFBDpbIVza8yc',
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENTID,
     callbackUrl: domain + '/auth/callback',
 }
