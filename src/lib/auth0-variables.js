b606e69e3e70ddb9b38077583db83ba9db099971
cdeon <clement.deon@data-publica.com>
2019-11-04 09:58:36 +0100

    #61 Add secrets to zeit project

--- src/lib/auth0-variables.js
+++ src/lib/auth0-variables.js
@@ -1,5 +1,6 @@
 const domain =
     process.env.ENV === 'dev' ? 'http://localhost:3000' : 'https://tottem.app'
+
 export const AUTH_CONFIG = {
     domain: process.env.AUTH0_DOMAIN,
     clientId: process.env.AUTH0_CLIENTID,
