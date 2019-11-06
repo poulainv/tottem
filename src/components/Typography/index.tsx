934e8228555a20a51437b5c931ffac4a7a248f85
Vincent Poulain <vincent.poulain2@gmail.com>
2019-11-06 16:35:35 +0100

    Make Collection name selectionable

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -92,6 +92,7 @@ export const ElementDescription = styled.p`
 export const CollectionTitle = styled.h2`
     font-size: 16px;
     font-weight: 400;
+    user-select: text;
     cursor: pointer;
     color: ${grey900};
 
