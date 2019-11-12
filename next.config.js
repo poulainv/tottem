ca35bb845b4abc43639be8f6874081c639595405
Vincent Poulain <vincent.poulain2@gmail.com>
2019-11-12 14:58:14 +0100

    Add webpacke bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 const { parsed: localEnv } = require('dotenv').config()
 const webpack = require('webpack')
 
-// to expose ENV variables to the client side
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
+    // to expose ENV variables to the client side
     webpack: (config, { dev }) => {
         config.plugins.push(
             new webpack.EnvironmentPlugin(localEnv || { ENV: 'prod' })
         )
         return config
     },
+
     env: {
         ENV: process.env.ENV,
         AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
         AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
     },
-}
+})
