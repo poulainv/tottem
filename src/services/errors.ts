e275cccb688d8e8f934547d39b187815f7f78ac1
Vincent Poulain <vincent.poulain2@gmail.com>
2019-11-26 19:25:38 +0100

    Handle specific errors differently

--- src/services/errors.ts
+++ src/services/errors.ts
@@ -1,4 +1,31 @@
 import notification from 'antd/lib/notification'
+import { ErrorHandler } from 'apollo-link-error'
+
+const handleGraphQLErrors: ErrorHandler = ({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+        graphQLErrors.forEach(({ message, locations, path, extensions }) => {
+            if (extensions !== undefined && extensions) {
+                if (extensions.code === 'FORBIDDEN') {
+                    openNotification(
+                        'Permissions insuffisantes',
+                        'Essayez de vous reconnecter et vérifiez quand vous avez les droits suffisants'
+                    )
+                }
+            } else {
+                openNotification(
+                    'Un petit problème de notre côté...',
+                    'Aïe ! Re-tentez le coup, sinon contactez-nous.'
+                )
+            }
+        })
+    }
+    if (networkError) {
+        openNotification(
+            'Toujours connecté ?',
+            'Aïe ! Re-tentez le coup, sinon contactez-nous.'
+        )
+    }
+}
 
 const openNotification = (title: string, message: string) => {
     const isServer = typeof window === 'undefined'
@@ -12,4 +39,4 @@ const openNotification = (title: string, message: string) => {
     }
 }
 
-export { openNotification }
+export { handleGraphQLErrors }
