3893d540b03a37e9887a78c0cf400074df8dd13f
Vincent Poulain <vincent.poulain2@gmail.com>
2019-12-17 13:06:04 +0100

    Tricky rebase

--- src/scenes/Me/Collection/index.tsx
+++ src/scenes/Me/Collection/index.tsx
@@ -1,35 +1,24 @@
 import range from 'lodash.range'
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { Facebook } from 'react-content-loader'
 import { useGetCollectionIdQuery } from '../../../generated/types'
 import LoadingPage from '../../LoadingPage'
 import ItemCard from '../ItemCard'
 import ItemForm from '../ItemForm'
 import HeaderForm from './HeaderForm'
+import { ItemType } from '../../common'
+import FilterBadges from './FilterBadges'
 
 interface Props {
     collectionId: string
     dispatch: (action: 'SAVED' | 'SAVING' | 'CHANGED') => void
 }
 
-<<<<<<< HEAD
 export default ({ dispatch, collectionId }: Props) => {
     const isBrowser = typeof window !== 'undefined'
-    const [selectedType, setSelectedType] = useState<ItemType | 'All'>('All')
-    const { data } = useGetCollectionIdQuery({
-=======
-export default ({ collectionId }: Props) => {
+
     const [selectedTypes, setSelectedTypes] = useState<Array<ItemType>>([])
-    const authUser = getAuthUser()
-
-    const [message, dispatch] = useStatusMessage()
-
-    if (authUser === undefined) {
-        return <LoadingPage />
-    }
-
-    const { data, loading } = useGetCollectionIdQuery({
->>>>>>> use reducer hook instead of state to filter collections on multiple types
+    const { data } = useGetCollectionIdQuery({
         variables: { collectionId },
         returnPartialData: isBrowser,
     })
@@ -37,14 +26,12 @@ export default ({ collectionId }: Props) => {
         return <LoadingPage />
     }
 
-    const { collection } = data
-
     const handleFilterChange = (filter: Array<ItemType>) => {
         setSelectedTypes(filter)
     }
+    const { collection } = data
 
     return (
-<<<<<<< HEAD
         <Fragment>
             <HeaderForm
                 collection={collection}
@@ -55,6 +42,12 @@ export default ({ collectionId }: Props) => {
                 // tslint:disable-next-line: jsx-no-lambda
                 onSaving={() => dispatch('SAVING')}
             />
+            <div>
+                <FilterBadges
+                    onFilterChange={handleFilterChange}
+                    items={collection.items}
+                />
+            </div>
             <div className="mt-8">
                 <ItemForm collectionId={collection.id} />
             </div>
@@ -70,64 +63,6 @@ export default ({ collectionId }: Props) => {
                     : range(4).map((i: number) => {
                           return <Facebook key={i} width={600} height={140} />
                       })}
-=======
-        <div className="flex h-screen text-sm ">
-            <Sidenav authUserId={authUser.id} />
-            <div className="flex flex-1 flex-col">
-                <TopBar
-                    message={message}
-                    avatar={authUser.picture}
-                    username={authUser.name}
-                />
-                <main className="text-sm">
-                    <HeaderForm
-                        collection={collection}
-                        onChange={() => dispatch('CHANGED')}
-                        onSaved={() => dispatch('SAVED')}
-                        onSaving={() => dispatch('SAVING')}
-                    />
-                    <p className="mx-1">Vincent</p>
-                    <p className="mx-1">|</p>
-                    <InlineIcon
-                        className="mx-1"
-                        height={14}
-                        width={14}
-                        icon={baselineSettings}
-                    />{' '}
-                </div>
-                <main className="text-sm w-full max-w-4xl mx-auto ">
-                    <Status>
-                        {(
-                            onSavedF: (collectionId: string) => void,
-                            onSaving: () => void
-                        ) => (
-                            <HeaderForm
-                                collection={collection}
-                                onSaved={onSavedF}
-                                onSaving={onSaving}
-                            />
-                        )}
-                    </Status>
-<<<<<<< HEAD
-=======
-
-                    <FilterBadges
-                        onFilterChange={handleFilterChange}
-                        items={collection.items}
-                    />
-                    {collection.items
-                        .filter(i => {
-                            return (
-                                !selectedTypes.length ||
-                                selectedTypes.includes(i.type)
-                            )
-                        })
-                        .map(i => {
-                            return <div>{i.id}</div>
-                        })}
->>>>>>> use reducer hook instead of state to filter collections on multiple types
-                </main>
->>>>>>> use reducer hook instead of state to filter collections on multiple types
             </div>
         </Fragment>
     )
