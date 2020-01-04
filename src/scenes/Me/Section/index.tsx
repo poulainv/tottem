b0cc3b55e6daf65f5074d7041c4635642849d265
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-04 16:51:22 +0100

    Add empty box when empty section

--- src/scenes/Me/Section/index.tsx
+++ src/scenes/Me/Section/index.tsx
@@ -4,6 +4,7 @@ import HeaderForm from './HeaderForm'
 import Options from './Options'
 import NewCollectionBtn from './NewCollectionBtn'
 import CollectionCard from './CollectionCard'
+import EmptyBox from '../../../../public/pictograms/empty-box.svg'
 
 interface SectionPageProps {
     sectionId: string
@@ -17,7 +18,7 @@ export default ({ sectionId }: SectionPageProps) => {
         return <div>Foo</div>
     }
     const { section } = data
-
+    const collections = section.collections.filter(x => !x.isDeleted)
     return (
         <div className="flex flex-col">
             <div className="flex flex-row justify-between">
@@ -38,9 +39,8 @@ export default ({ sectionId }: SectionPageProps) => {
                 </div>
             </div>
             <div className="pt-16">
-                {section.collections
-                    .filter(x => !x.isDeleted)
-                    .map(collection => {
+                {collections.length !== 0 ? (
+                    collections.map(collection => {
                         return (
                             <CollectionCard
                                 collection={collection}
@@ -49,7 +49,17 @@ export default ({ sectionId }: SectionPageProps) => {
                                 className="mt-8 first:mt-0"
                             />
                         )
-                    })}
+                    })
+                ) : (
+                    <div className="w-full mt-40 flex items-center justify-center">
+                        <div className="flex flex-col items-center">
+                            <EmptyBox className="fill-current h-20 w-20 text-gray-400" />
+                            <span className="text-gray-600 mt-4">
+                                Let's your first collection
+                            </span>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
