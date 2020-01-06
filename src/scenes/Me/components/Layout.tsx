4247923f2bd65c90c0fe19e706a492974e5ed9c1
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-06 19:41:51 +0100

    Layout main better responsive

--- src/scenes/Me/components/Layout.tsx
+++ src/scenes/Me/components/Layout.tsx
@@ -26,7 +26,7 @@ export default ({ loggedInUser, children }: Props) => {
                     avatar={loggedInUser.picture}
                     username={loggedInUser.name}
                 />
-                <main className="text-sm w-full max-w-4xl mx-auto mt-2 pb-16">
+                <main className="text-sm w-full max-w-2xl xl:max-w-4xl mx-16 xl:mx-auto mt-2 pb-16">
                     {children(dispatch)}
                 </main>
             </div>
