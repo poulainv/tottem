af6eafc08d12073139081248104eebcfd9e6c45a
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-09 16:09:57 +0100

    Add transition item form

--- src/scenes/Me/components/AddItemForm/index.tsx
+++ src/scenes/Me/components/AddItemForm/index.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import AddButton, { AddActions } from '../../../../components/AddButtonItem'
 import Spinner from '../../../../components/Spinner'
 import { Dispatch } from 'react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default ({
     isLoading,
@@ -25,7 +26,19 @@ export default ({
                     <AddButton onSelect={dispatch} />
                 )}
             </div>
-            {isShow && FormItem}
+            <TransitionGroup>
+                {isShow && (
+                    <CSSTransition
+                        classNames={{
+                            enter: 'transform scale-y-50',
+                            enterDone: 'transition-all duration-200',
+                        }}
+                        timeout={{ enter: 0, exit: 0 }}
+                    >
+                        {FormItem}
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     )
 }
