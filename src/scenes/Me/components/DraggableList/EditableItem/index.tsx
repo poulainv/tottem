9a5b1892a7206a65c6bf02120fd4b96bb47fe3f1
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-10 15:09:31 +0100

    Make item cover image more responsive

--- src/scenes/Me/components/DraggableList/EditableItem/index.tsx
+++ src/scenes/Me/components/DraggableList/EditableItem/index.tsx
@@ -22,7 +22,11 @@ const EditableItem: React.FunctionComponent<IEditableItemProps &
         <div className="shadow rounded-sm flex flex-row flex-shrink-0 w-full min-h-9">
             <div className="bg-gray-100 w-1/2 p-4 flex flex-col">
                 <div className="flex flex-row items-start">
-                    <a href={item.productUrl} className="w-24" target="_blank">
+                    <a
+                        href={item.productUrl}
+                        className="w-16 xl:w-24 flex-shrink-0"
+                        target="_blank"
+                    >
                         <CoverImage item={item} />
                     </a>
                     <div className="flex flex-col py-1 mx-4 w-64">
