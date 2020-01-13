8117fd9229b5d7301b6461565c16b1358c42054d
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-13 13:10:43 +0100

    Ensure max-height topbar profile

--- src/scenes/Profile/components/TopBar.tsx
+++ src/scenes/Profile/components/TopBar.tsx
@@ -9,12 +9,19 @@ interface ITopBarProps {
 
 const TopBar: React.FunctionComponent<ITopBarProps> = ({}) => {
     return (
-        <div className="w-full px-2 h-8 flex justify-between items-center text-gray-800 leading-none">
-            <Link as="/me" href="/me">
-                <a className="flex items-center h-6 text-brand-800 border border-brand-800 rounded px-2 text-xs">
-                    Edit profile
-                </a>
-            </Link>
+        <div className="w-full px-2 h-8 flex justify-between items-center text-gray-800 leading-none flex-shrink-0">
+            <div className="flex items-center">
+                <Link as="/me" href="/me">
+                    <a className="flex items-center h-6 text-brand-800 border border-brand-800 rounded px-2 text-xs hover:shadow hover:text-brand-800">
+                        Edit profile
+                    </a>
+                </Link>
+                <div className="text-gray-500 ml-4">
+                    <span>Vincent / </span>
+                    <span> Musique / </span>
+                    <span>Manipulation ...</span>
+                </div>
+            </div>
             <div className="flex justify-end items-center text-gray-800 flex-shrink-0">
                 <p className="mx-1">Share</p>
                 <span className="text-gray-400">|</span>
