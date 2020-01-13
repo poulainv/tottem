d9d534a054a582761812966477e62dd5d4bcb964
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-13 11:02:04 +0100

    Add Collection Page

---
+++ src/scenes/Profile/Collection/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export interface ICollectionProps {
+    authUserId?: string
+    collectionId: string
+    profileSlug: string
+}
+
+export default function Collection(props: ICollectionProps) {
+    return <div />
+}
