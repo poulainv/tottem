5920396487d391e0849e59d7a05255f0f5cd6276
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-17 13:30:06 +0100

    Clean up landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,28 @@ import { StyledButton } from '../components/Button'
 export default () => {
     return (
         <div style={{ backgroundColor: '#FFFFF9' }} className="h-screen w-full">
-            <div className="flex flex-col max-w-4xl mx-auto pt-20 justify-between items-center h-full">
+            <div className="flex flex-col max-w-4xl mx-auto pt-10 justify-between items-center h-full">
+                <div className="flex justify-between items-center w-full">
+                    <img
+                        className="h-6 self-start"
+                        src="/logo.svg"
+                        alt="Tottem logo"
+                    />
+                    <h1 className="text-2xl">Tottem</h1>
+                </div>
                 <h1 className="text-5xl text-gray-800 font-regular text-center">
                     All the content you love, in one place
                 </h1>
-                <p className="text-gray-700 text-2xl font-thin">
-                    Tottem is your new library. Smart, social and beautiful.
-                </p>
-                <Link as="/auth/login" href="/auth/login">
-                    <a className="w-32">
-                        <StyledButton> Get started </StyledButton>
-                    </a>
-                </Link>
+                <div className="flex flex-col items-center">
+                    <p className="text-gray-700 text-2xl font-thin">
+                        Tottem is your new library. Smart, social and beautiful.
+                    </p>
+                    <Link as="/auth/login" href="/auth/login">
+                        <a className="w-32 mt-4">
+                            <StyledButton> Get started </StyledButton>
+                        </a>
+                    </Link>
+                </div>
                 <img
                     className="rounded mt-10 shadow-lg"
                     src="screen.png"
