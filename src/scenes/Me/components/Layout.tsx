cc8994b49e31fb74190da3001e7a1ef5783d543a
Vincent Poulain <vincent.poulain2@gmail.com>
2020-01-22 03:45:56 +0100

    Make mobile experience better

--- src/scenes/Me/components/Layout.tsx
+++ src/scenes/Me/components/Layout.tsx
@@ -20,12 +20,12 @@ export default ({ loggedInUser, children }: Props) => {
         <div className="flex h-screen text-sm font-sans">
             <Sidenav authUserId={loggedInUser.id} />
             <NavigateModal authUserId={loggedInUser.id} />
-            <div className="flex flex-1 flex-col overflow-auto h-full">
+            <div className="flex flex-1 flex-col lg:overflow-auto h-full">
                 <TopBar
                     avatar={loggedInUser.picture}
                     username={loggedInUser.name}
                 />
-                <main className="text-sm w-full max-w-2xl xl:max-w-4xl mx-16 xl:mx-auto mt-2 pb-16">
+                <main className="text-sm lg:w-full max-w-2xl xl:max-w-4xl mx-16 xl:mx-auto mt-2 pb-16">
                     {children(loggedInUser.id)}
                 </main>
             </div>
